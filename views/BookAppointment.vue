5c05db88e988be29c0191a5a6df07f7515a835ca
heba <hebasuliman273@gmail.com>
2025-06-19 16:23:18 +0300

    edit URL

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://for6xj9hok.execute-api.us-east-1.amazonaws.com/Booking/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
@@ -62,7 +62,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+      fetch("POST https://for6xj9hok.execute-api.us-east-1.amazonaws.com/Booking/appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
